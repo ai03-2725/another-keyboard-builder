6d70926060992a61146cb6c5b1a9be59d4ff27a7
ai03 <sarcaphx510@gmail.com>
2018-10-21 21:37:50 -0700

    Begin basic stab support

--- plategen.py
+++ plategen.py
@@ -16,6 +16,7 @@ modelspace = plate.modelspace()
 # Cutout parameters
 cutout_type = "mx"
 cutout_radius = Decimal('0.5')
+stab_type = "mx"
 
 # Unit size (i.e. 1U = 19.05mm)
 unit_width = Decimal('19.05')
@@ -23,17 +24,19 @@ unit_height = Decimal('19.05')
 
 # Debug parameters:
 # Draw key outlines?
-debug_draw_key_outline = True
+debug_draw_key_outline = False
 # Use generic matrix?
 debug_use_generic_matrix = True
 # Tell user what's going on when generating?
 debug_log = True
 # Debug matrix data
-debug_matrix_data = """["~\n`","!\n1","@\n2","#\n3","$\n4","%\n5","^\n6","&\n7","*\n8","(\n9",")\n0","_\n-","+\n=",{w:2},"Backspace"],
-[{w:1.5},"Tab","Q","W","E","R","T","Y","U","I","O","P","{\n[","}\n]",{w:1.5},"|\n\\"],
-[{w:1.75},"Caps Lock","A","S","D","F","G","H","J","K","L",":\n;","\"\n'",{w:2.25},"Enter"],
-[{w:2.25},"Shift","Z","X","C","V","B","N","M","<\n,",">\n.","?\n/",{w:2.75},"Shift"],
-[{w:1.25},"Ctrl",{w:1.25},"Win",{w:1.25},"Alt",{a:7,w:6.25},"",{a:4,w:1.25},"Alt",{w:1.25},"Win",{w:1.25},"Menu",{w:1.25},"Ctrl"]"""
+debug_matrix_data = """
+[{a:7},"","","","","","","","","","","","","",{w:2},""],
+[{w:1.5},"","","","","","","","","","","","","",{w:1.5},""],
+[{w:1.75},"","","","","","","","","","","","",{w:2.25},""],
+[{w:2.25},"","","","","","","","","","","",{w:2.75},""],
+[{w:1.25},"",{w:1.25},"",{w:1.25},"",{w:6.25},"",{w:1.25},"",{w:1.25},"",{w:1.25},"",{w:1.25},""]
+"""
 
 
 #== Runtime-modified variables
@@ -87,6 +90,70 @@ def is_a_number(s):
 		return_value = False
 	return return_value
 	
+# Stab cutout maker
+# The x and y are center top, like this:
+#
+# ---X---
+# |     |
+# |     |
+# |     |
+# |_   _|
+#   |_|
+
+def make_stab_cutout(x, y):
+	if (stab_type == "mx-simple"):
+		# Rectangular simplified
+		modelspace.add_line((x - Decimal('3.3274'), y), (x + Decimal('3.3274'), y))
+		modelspace.add_line((x - Decimal('3.3274'), y - Decimal('13.462')), (x + Decimal('3.3274'), y - Decimal('13.462')))
+		modelspace.add_line((x - Decimal('3.3274'), y), (x - Decimal('3.3274'), y - Decimal('13.462')))
+		modelspace.add_line((x + Decimal('3.3274'), y), (x + Decimal('3.3274'), y - Decimal('13.462')))
+	elif (stab_type == "mx"):
+		# Proper MX
+		# Do X first, then Y second
+		modelspace.add_line((x - Decimal('3.3274'), y), (x + Decimal('3.3274'), y))
+		modelspace.add_line((x - Decimal('3.3274'), y - Decimal('12.2936')), (x - Decimal('1.8034'), y - Decimal('12.2936')))
+		modelspace.add_line((x + Decimal('3.3274'), y - Decimal('12.2936')), (x + Decimal('1.8034'), y - Decimal('12.2936')))
+		modelspace.add_line((x - Decimal('1.8034'), y - Decimal('13.462')), (x + Decimal('1.8034'), y - Decimal('13.462')))
+		
+		modelspace.add_line((x - Decimal('3.3274'), y), (x - Decimal('3.3274'), y - Decimal('12.2936')))
+		modelspace.add_line((x + Decimal('3.3274'), y), (x + Decimal('3.3274'), y - Decimal('12.2936')))
+		modelspace.add_line((x - Decimal('1.8034'), y - Decimal('12.2936')), (x - Decimal('1.8034'), y - Decimal('13.462')))
+		modelspace.add_line((x + Decimal('1.8034'), y - Decimal('12.2936')), (x + Decimal('1.8034'), y - Decimal('13.462')))
+		
+	else:
+		print("Unsupported stab type.", file=sys.stderr)
+		print("Stab types: mx, mx-simple", file=sys.stderr)
+		exit(1)
+	
+# Calls make stab cutout based on unit width and style
+def generate_stabs(x, y, unitwidth):
+	if (stab_type == "mx-simple" or stab_type == "mx"):
+		stab_y = y - Decimal('1.2954')
+		center_x = x + (cutout_width / Decimal('2'))
+		if (debug_log):
+			print("Genstabs: width " + str(unitwidth))
+		# Switch based on unit width
+		if (unitwidth >= 8): 
+			make_stab_cutout(center_x + Decimal('66.675'), stab_y)
+			make_stab_cutout(center_x - Decimal('66.675'), stab_y)
+		elif (unitwidth >= 7): 
+			make_stab_cutout(center_x + Decimal('57.15'), stab_y)
+			make_stab_cutout(center_x - Decimal('57.15'), stab_y)
+		elif (unitwidth == 6.25): 
+			make_stab_cutout(center_x + Decimal('50'), stab_y)
+			make_stab_cutout(center_x - Decimal('50'), stab_y)
+		elif (unitwidth == 6): 
+			make_stab_cutout(center_x + Decimal('38.1'), stab_y)
+			make_stab_cutout(center_x - Decimal('57.15'), stab_y)
+		elif (unitwidth >= 3): 
+			make_stab_cutout(center_x + Decimal('19.05'), stab_y)
+			make_stab_cutout(center_x - Decimal('19.05'), stab_y)
+		elif (unitwidth >= 2): 
+			make_stab_cutout(center_x + Decimal('11.938'), stab_y)
+			make_stab_cutout(center_x - Decimal('11.938'), stab_y)
+		
+	
+	
 #== The code
 
 # Generate switch cutout sizes
@@ -99,7 +166,7 @@ elif (cutout_type == "alps"):
 else:
 	print("Unsupported cutout type.")
 	print("Supported: mx, alps")
-	exit()
+	exit(1)
 
 # If debug matrix is on, make sth generic
 if (debug_use_generic_matrix):
@@ -107,6 +174,18 @@ if (debug_use_generic_matrix):
 # Otherwise, take from stdin
 else:
 	input_data = sys.stdin.read()
+	
+# Sanitize by removing \" (KLE's literal " for a label)
+input_data = input_data.replace('\n', '')
+input_data = input_data.replace(r'\"', '')
+
+# TODO: Filter out improper quotes from " being in a label!
+
+if (debug_log):
+	print("Filtered input data:")
+	print(input_data)
+	print("")
+
 
 # Make some variables
 in_row = False
@@ -117,6 +196,8 @@ parsing_string = ""
 in_label = False
 current_width = Decimal(1)
 current_height = Decimal(1)
+max_width = Decimal(0)
+max_height = Decimal(0)
 	
 # Act upon the input data
 for c in input_data:
@@ -126,7 +207,7 @@ for c in input_data:
 		# If already in a row, bad formatting!
 		if(in_row):
 			print("Malformed input: Row in a row", file=sys.stderr)
-			exit()
+			exit(1)
 		
 		in_row = True
 		
@@ -138,11 +219,21 @@ for c in input_data:
 		# If not in a row, bad formatting!
 		if(not in_row):
 			print("Malformed input: Row ends outside a row", file=sys.stderr)
-			exit()
+			exit(1)
 		
 		in_row = False
+		
 		# Move down a row
 		current_y -= unit_height;
+		
+		# Check for max width/height for drawing outlines
+		if (current_x > max_width):
+			max_width = current_x
+		
+		if (abs(current_y) > abs(max_height)):
+			max_height = current_y
+		
+		# Reset x to 0
 		current_x = Decimal('0');
 		
 		if (debug_log):
@@ -153,7 +244,7 @@ for c in input_data:
 		# If are already parsing, bad data!
 		if(in_data):
 			print("Malformed input: data begins in data", file=sys.stderr)
-			exit()
+			exit(1)
 		
 		in_data = True
 		
@@ -165,7 +256,7 @@ for c in input_data:
 		# If are not already parsing, bad data!
 		if(not in_data):
 			print("Malformed input: data ends outside of data", file=sys.stderr)
-			exit()
+			exit(1)
 			
 		# We could have been parsing a width or height.
 		# If so, apply changes.
@@ -175,7 +266,7 @@ for c in input_data:
 			if (not is_a_number(parsing_string)):
 				print(parsing_string)
 				print("Malformed input: width input isn't a digit", file=sys.stderr)
-				exit()
+				exit(1)
 		
 			current_width = Decimal(parsing_string)
 			parsing_string = ""
@@ -190,7 +281,7 @@ for c in input_data:
 			if (not is_a_number(parsing_string)):
 				print(parsing_string)
 				print("Malformed input: height input isn't a digit", file=sys.stderr)
-				exit()
+				exit(1)
 		
 			current_height = Decimal(parsing_string)
 			parsing_string = ""
@@ -221,7 +312,13 @@ for c in input_data:
 				if (debug_draw_key_outline):
 					make_key_outline(current_x, current_y, current_width, current_height)
 				# Now we draw the switch cutout
-				make_cutout(current_x + (((current_width * unit_width) - cutout_width) / Decimal('2')), current_y - (((current_height * unit_height) - cutout_height) / Decimal('2')))
+				cutout_corner_x = current_x + (((current_width * unit_width) - cutout_width) / Decimal('2'))
+				cutout_corner_y = current_y - (((current_height * unit_height) - cutout_height) / Decimal('2'))
+				make_cutout(cutout_corner_x, cutout_corner_y)
+				
+				# Generate stabs
+				generate_stabs(cutout_corner_x, cutout_corner_y, current_width)
+				
 				# Move current coords over:
 				current_x += current_width * unit_width;
 				# And reset size to normal:
@@ -229,7 +326,8 @@ for c in input_data:
 				current_height = Decimal(1);
 				
 				if (debug_log):
-					print("Ended label, drew switch")
+					print("Ended label, drew switch.")
+				
 		
 	elif (c == 'w' and in_data):
 		# Width data.
@@ -238,7 +336,7 @@ for c in input_data:
 		#But if already parsing sth else, malformed string
 		if (parsing_width or parsing_height):
 			print("Malformed input: width begins when already parsing size variable", file=sys.stderr)
-			exit()
+			exit(1)
 		
 		parsing_width = True
 		
@@ -252,7 +350,7 @@ for c in input_data:
 		#But if already parsing sth else, malformed string
 		if (parsing_width or parsing_height):
 			print("Malformed input: height begins when already parsing size variable", file=sys.stderr)
-			exit()
+			exit(1)
 		
 		parsing_height = True
 		
@@ -276,7 +374,7 @@ for c in input_data:
 			if (not is_a_number(parsing_string)):
 				print(parsing_string)
 				print("Malformed input: width input isn't a digit", file=sys.stderr)
-				exit()
+				exit(1)
 		
 			current_width = Decimal(parsing_string)
 			parsing_string = ""
@@ -291,7 +389,7 @@ for c in input_data:
 			if (not is_a_number(parsing_string)):
 				print(parsing_string)
 				print("Malformed input: height input isn't a digit", file=sys.stderr)
-				exit()
+				exit(1)
 		
 			current_height = Decimal(parsing_string)
 			parsing_string = ""
@@ -311,8 +409,10 @@ for c in input_data:
 		else:
 		
 			if (debug_log):
-				print("Ignoring " + c)
-				
+				if(in_label):
+					print("Part of label: " + c)
+				else:
+					print("Ignoring " + c)
 			continue
 			
 # At this point we should be done. Are we?
@@ -320,6 +420,14 @@ if (in_row or in_data or in_label or parsing_width or parsing_height):
 	print(parsing_string, file=sys.stderr)
 	print("Malformed input: Ends abruptly", file=sys.stderr)
 	
-	exit()
+	plate.saveas('error-plate.dxf')
+	
+	exit(1)
+	
+# Draw outer bounds - top, bottom, left, right
+modelspace.add_line((0,0), (max_width, 0))
+modelspace.add_line((0,max_height), (max_width, max_height))
+modelspace.add_line((0,0), (0, max_height))
+modelspace.add_line((max_width,0), (max_width, max_height))
 	
 plate.saveas('plate.dxf')

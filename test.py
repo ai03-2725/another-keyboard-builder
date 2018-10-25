a7bff6d431242e037a822b2636ac90762a566f96
ai03 <sarcaphx510@gmail.com>
2018-10-25 14:43:52 -0700

    Clean up directory

--- test.py
+++ test.py
@@ -2,8 +2,8 @@
 
 import plategen
 
-filename = 'test-full104'
-gen = plategen.PlateGenerator()
+filename = 'test-data/test-full104'
+gen = plategen.PlateGenerator('mx', '0.5', 'mx-simple', '0.5', 'none', '0.5', '19.05', '19.05', 'stdout', 'plate.dxf', False)
 with open(filename, 'r') as input_file:
     input_data = input_file.read()
     gen.generate_plate(input_data)

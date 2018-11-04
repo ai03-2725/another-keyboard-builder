3274ba4b3e1d6d95ea6817ede4d2eea5ffa35301
ai03 <sarcaphx510@gmail.com>
2018-11-04 13:58:09 -0800

    Begin web frontend work

--- web.py
+++ web.py
@@ -1,30 +1,34 @@
-from Flask import Flask, render_template, flash, request
+from flask import Flask, render_template, flash, request, send_from_directory
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 
 # App config.
 DEBUG = True
 app = Flask(__name__)
 app.config.from_object(__name__)
-#app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
+app.config['SECRET_KEY'] = 'change me'.encode('utf8')
  
 class PlateGenFrontend(Form):
     kle_form = TextField('KLE Raw Data', validators=[validators.required()])
  
+@app.route('/img/<path:path>')
+def static_img(path):
+    return send_from_directory('img', path)
+
 @app.route("/", methods=['GET', 'POST'])
 def hello():
     form = PlateGenFrontend(request.form)
  
-    print form.errors
-    if request.method == 'POST':
-        kle_rawdata = request.form['kle_form']
-        print name
+    print (form.errors)
+    if (request.method == 'POST'):
+        kle_rawdata = request.form['kle-data']
+        print (kle_rawdata)
  
-        if form.validate():
+        if (form.validate()):
             flash('KLE string stored')
         else:
-            flash('Please ')
+            flash('Error: Please check the data fields.')
  
-    return render_template('hello.html', form=form)
+    return render_template('base.html', form=form)
  
-if __name__ == "__main__":
+if (__name__ == "__main__"):
     app.run()

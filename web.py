da300dde5469f92a60af5d6e90a75c3373043896
ai03 <sarcaphx510@gmail.com>
2018-11-04 17:40:47 -0800

    Remove wtforms

--- web.py
+++ web.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, flash, request, send_from_directory
-from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 
 # App config.
 DEBUG = True
@@ -7,28 +6,14 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = 'change me'.encode('utf8')
  
-class PlateGenFrontend(Form):
-    kle_form = TextField('KLE Raw Data', validators=[validators.required()])
- 
 @app.route('/img/<path:path>')
 def static_img(path):
     return send_from_directory('img', path)
 
-@app.route("/", methods=['GET', 'POST'])
-def hello():
-    form = PlateGenFrontend(request.form)
- 
-    print (form.errors)
-    if (request.method == 'POST'):
-        kle_rawdata = request.form['kle-data']
-        print (kle_rawdata)
- 
-        if (form.validate()):
-            flash('KLE string stored')
-        else:
-            flash('Error: Please check the data fields.')
- 
-    return render_template('base.html', form=form)
+@app.route("/", methods=['GET'])
+def default_route():
+
+    return render_template('base.html')
  
 if (__name__ == "__main__"):
     app.run()

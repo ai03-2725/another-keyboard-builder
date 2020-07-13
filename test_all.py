ea5e5f70ac914383af470b6f497771ce21b7c3d0
Urs Wihlfahrt <urs.w@gmx.de>
2020-07-13 21:18:13 +0200

    added KLE presets
    prepare for
    * anaconda
    * vscode

---
+++ test_all.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+import os
+import codecs
+import plategen
+from pathlib import Path
+
+
+basepath = Path(__file__).parent.absolute()  
+path_input = basepath / 'test-data'
+path_output = basepath / 'test-writedir'
+
+
+
+dia_exe = r"C:\Users\Anwender\portableApps\DiaPortable\DiaPortable.exe"
+'''
+Path to gnome dia or None if no PNG file should be created
+''' 
+all_file_names = path_input.glob('*')
+
+all_file_names = [ path_input / 'alice-urwi_red2' ]
+# all_file_names.append( path_input / 'alice-urwi' )
+
+
+
+for filename in all_file_names:
+    dxf_out = path_output / (filename.stem + '.dxf')
+    png_out = dxf_out.with_suffix('.png')
+
+    gen = plategen.PlateGenerator('mx', '0.5', 'mx-simple', '0.5', 'none', '0.5', '19.05', '19.05', True)
+
+    print( ' EXECUTE {}'.format(filename.name))
+
+    with  codecs.open(filename, 'r',  "utf-8") as input_file, open(dxf_out, 'w') as fout:
+        input_data = input_file.read()
+        gen.generate_plate(fout, input_data)
+
+        if dia_exe is not None:
+            popen_strg = r'"{}"  "{}" -e "{}"'.format(dia_exe, dxf_out, png_out)
+            print(" EXECUTE " + popen_strg)
+            os.popen(popen_strg)
+
+
